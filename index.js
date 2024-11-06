b5e5f60bd393c809fd173d4e52d41eaed50ebd77
anubhavshrivastav <shrivastavanubhav0@gmail.com>
2024-11-06 19:06:12 +0530

    add logic

---
+++ index.js
@@ -0,0 +1,52 @@
+// step 1 - Selecting all the elements 
+const initialPrice = document.querySelector("#initial-price");
+const noOfShares = document.querySelector("#noStocks");
+const currentPrice = document.querySelector("#currentPrice");
+const submit = document.querySelector("#submit").addEventListener('click',calculate)
+const output = document.querySelector("#output");
+
+function calculate(){
+  
+  let costPrice = Number(initialPrice.value);
+  let noShares = Number(noOfShares.value);
+  let sellingPrice = Number(currentPrice.value);
+  
+
+  if(costPrice && noShares && sellingPrice !== null){
+    
+    if(sellingPrice > costPrice ){
+        let profit = sellingPrice - costPrice;
+        let profitPercentage = (profit / costPrice) * 100;
+        output.innerText = `Yay!! your profit is ${profit} and profit percentage is ${profitPercentage}%.`
+    }else if(sellingPrice < costPrice){
+        let loss = costPrice - sellingPrice;
+        let lossPercentage = (loss / costPrice) * 100;
+        output.innerText = `whoops!! your loss is ${loss} and loss percentage is ${lossPercentage}%.`
+    }else if(sellingPrice === costPrice){
+        output.innerText = `No pain No gain,and no gain no pain`;
+    }
+    
+    
+  }else{
+       output.innerText = "Please Fill Out All Field.";
+  }  
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
